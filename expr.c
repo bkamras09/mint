6459fea77f0d5a084c3e19f00573236d9a2322b4
Brian Kamras <bkamras09@gmail.com>
2023-12-08 16:34:50 -0600

    pushing to make sure nothing is lost after a reinstall

--- expr.c
+++ expr.c
@@ -7,6 +7,117 @@
 #include <errno.h>
 #include "math.h"
 
+
+AstNode *expr_AstNode(node **n);
+AstNode *term_AstNode(node **n);
+AstNode *factor_AstNode(node **n);
+AstNode *exponent_AstNode(node **n);
+
+AstNode *new_AstNode_Operand(float value) {
+    AstNode *n = malloc(sizeof(AstNode));
+    if (!n) return NULL;
+    n->left = n->right = NULL;
+    n->type = DIGIT;
+    n->value = value;
+    snprintf(n->token, sizeof(n->token), "%f", value);
+    return n;
+}
+
+AstNode *new_AstNode_Operator(ETokenType type, AstNode *left, AstNode *right) {
+    AstNode *n = malloc(sizeof(AstNode));
+    if (!n) return NULL;
+    n->left = left;
+    n->right = right;
+    n->type = type;
+    // Token representation for operators is typically a single character
+    snprintf(n->token, sizeof(n->token), "%c", type); // Assuming type is a char representing the operator
+    return n;
+}
+
+AstNode *factor_AstNode(node **n) {
+    if ((*n)->token_type == DIGIT) {
+        float value = atof((*n)->token);
+        AstNode *operand = new_AstNode_Operand(value);
+        *n = (*n)->next;  // Move to the next token
+        return operand;
+    } else if ((*n)->token_type == LP) {
+        *n = (*n)->next;  // Move past the '(' token
+        AstNode *subExpr = expr_AstNode(n);  // Recursively build the AST for the sub-expression
+        if ((*n)->token_type != RP) {
+            fprintf(stderr, "Error: Expected right parenthesis\n");
+            exit(EXIT_FAILURE);
+        }
+        *n = (*n)->next;  // Move past the ')' token
+        return subExpr;
+    } else {
+        // ... Handle variables and other types of factors
+    }
+}
+
+AstNode *exponent_AstNode(node **n) {
+    AstNode *base = factor_AstNode(n);
+    while (*n != NULL && (*n)->token_type == EXP) {
+        *n = (*n)->next;  // Move to the exponent
+        AstNode *exp = exponent_AstNode(n);  // Recursively build the AST for the exponent
+        base = new_AstNode_Operator(EXP, base, exp);
+    }
+    return base;
+}
+
+AstNode *term_AstNode(node **n) {
+    AstNode *result = exponent_AstNode(n);
+    while (*n != NULL && ((*n)->token_type == MUL || (*n)->token_type == DIV)) {
+        ETokenType opType = (*n)->token_type;
+        *n = (*n)->next;  // Move to the next token
+        AstNode *rhs = exponent_AstNode(n);  // Build the AST for the right-hand side
+        result = new_AstNode_Operator(opType, result, rhs);
+    }
+    return result;
+}
+
+AstNode *expr_AstNode(node **n) {
+    AstNode *result = term_AstNode(n);
+    while (*n != NULL && ((*n)->token_type == PLUS || (*n)->token_type == MINUS)) {
+        ETokenType opType = (*n)->token_type;
+        *n = (*n)->next;  // Move to the next token
+        AstNode *rhs = term_AstNode(n);  // Build the AST for the right-hand side
+        result = new_AstNode_Operator(opType, result, rhs);
+    }
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 AstNode *new_AstNode(char p[]) {
 	AstNode *n = malloc(sizeof(AstNode));
 	if (!n) return NULL;
@@ -151,3 +262,7 @@ else {
 }
 # + put the lex_operator() inside the while (!end) loop, at the beginning
 */
+
+
+
+
